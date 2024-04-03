c8ab6615bc1e2c133b0e08523a209c10997bb68b
Niklas Lengert <s0563290@htw-berlin.de>
2024-04-03 22:01:35 +0200

    Day 01 coding challenge push

---
+++ day-1-printing-start.py
@@ -0,0 +1,46 @@
+# print("Hallo World!")
+
+#print(f"1. Mix {500}g of Flour, {10}g Yeast and {300}ml of water in a bowl.")
+#print(f"2. Knead the dough for {10} minutes.")
+#print(f"3. Add {3}g of Salt.")
+#print(f"4. Leave to rise for {2} hours.")
+#print(f"5. Bake at {200} degrees for {30} minutes.")
+
+#print("Hello world!\nHello world!\nHello world!\nHello world!\nHello world!")
+
+#print("Hello" + " " + "Niklas")
+
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the " + '"+"' + " sign.")
+# print('e.g. + print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# print("Hello " + input("What is your name?"))
+# print(len(input("What is your name?")))
+
+# name = "Jack"
+# print(name)
+
+# name = "Angela"
+# print(name)
+
+# name = input("What is your name?")
+# length = len(name)
+# print(length)
+
+###Coding Exercise
+# a = input()
+# b = input()
+
+# temp = a
+# a = b
+# b = temp
+
+# print("a: " + a)
+# print("b: " + b)
+
+###final
+# print("Welcome to the Band Name Generator.")
+# city_name = input("What's name of the city you grew up in?\n")
+# pet_name = input("What's your pet's name?\n")
+# print("Your band name could be " + city_name + " " + pet_name)
